b03304582ef34504adde69ff3d8ebbb51e438644
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-05-05 18:14:06 -0400

    Migrated basic pathfinding from old project

---
+++ src/Battle.Logic/PathFinding/PathResult.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+
+namespace Battle.PathFinding
+{
+    public class PathResult
+    {
+
+        public List<Tile> Tiles;
+        public List<Point> Path;
+
+        public Tile GetLastTile()
+        {
+            if (Tiles != null && Tiles.Count > 0)
+            {
+                return Tiles[^1]; //"^1" is the same asa "Tiles.Count - 1"
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public PathResult()
+        {
+            Tiles = new List<Tile>();
+            Path = new List<Point>();
+        }
+    }
+}
