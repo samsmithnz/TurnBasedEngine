d6b46d173fe6e18cb20adccb58b8a63d81321d6e
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-05-16 12:29:51 -0400

    Creating new turn structure

---
+++ src/Battle.Logic/MainGame/Game.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Battle.Logic.MainGame
+{
+    public class Game
+    {
+        public int TurnNumber { get; set; }
+        public List<Team> Teams { get; set; }
+        public string[,] Map { get; set; }
+    }
+}
