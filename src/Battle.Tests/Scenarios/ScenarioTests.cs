4463c5d4babf5f5e4335da33dddbdc1997e8b0f7
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-05-21 08:48:48 -0400

    Added scenario to show that field of view updates when cover is removed.

--- src/Battle.Tests/Scenarios/ScenarioTests.cs
+++ src/Battle.Tests/Scenarios/ScenarioTests.cs
@@ -8,6 +8,7 @@ using Battle.Tests.Characters;
 using Battle.Tests.Map;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Battle.Tests.Scenarios
 {
@@ -25,20 +26,20 @@ namespace Battle.Tests.Scenarios
             game.Map[6, 5] = CoverType.FullCover;
             game.Map[20, 11] = CoverType.FullCover;
             Character fred = CharacterPool.CreateFredHero();
+            fred.Location = new(5, 0, 5);
             Team team1 = new()
             {
                 Name = "Good guys",
                 Characters = new() { fred }
             };
-            fred.Location = new(5, 0, 5);
             game.Teams.Add(team1);
             Character jeff = CharacterPool.CreateJeffBaddie();
+            jeff.Location = new(20, 0, 10);
             Team team2 = new()
             {
                 Name = "Bad guys",
                 Characters = new() { jeff }
             };
-            jeff.Location = new(20, 0, 10);
             game.Teams.Add(team2);
             Queue<int> diceRolls = new(new List<int> { 100, 100, 0, 0, 100, 100, 100 }); //Chance to hit roll, damage roll, critical chance roll
 
@@ -200,6 +201,198 @@ Fred is ready to level up
 
             //Assert
             Assert.AreEqual(-5, jeff.Hitpoints);
+        }
+
+        //Enemy is behind cover and not visible.
+        //Throw grenade to destory cover and make him visible.
+        [TestMethod]
+        public void JeffIsHidingBehindCoverScenarioTest()
+        {
+            //Arrange
+            Game game = new();
+            game.TurnNumber = 1;
+            game.Map = MapUtility.InitializeMap(50, 50);
+            game.Map[5, 6] = CoverType.FullCover;
+            game.Map[14, 5] = CoverType.FullCover;
+            game.Map[14, 6] = CoverType.FullCover;
+            game.Map[14, 7] = CoverType.FullCover;
+            game.Map[14, 8] = CoverType.FullCover;
+            game.Map[14, 9] = CoverType.FullCover;
+            game.Map[14, 10] = CoverType.FullCover;
+            game.Map[14, 11] = CoverType.FullCover;
+            game.Map[14, 12] = CoverType.FullCover;
+            game.Map[14, 13] = CoverType.FullCover;
+            game.Map[14, 14] = CoverType.FullCover;
+            Character fred = CharacterPool.CreateFredHero();
+            fred.Location = new(5, 0, 5);
+            Team team1 = new()
+            {
+                Name = "Good guys",
+                Characters = new() { fred }
+            };
+            game.Teams.Add(team1);
+            Character jeff = CharacterPool.CreateJeffBaddie();
+            jeff.Location = new(15, 0, 10);
+            Team team2 = new()
+            {
+                Name = "Bad guys",
+                Characters = new() { jeff }
+            };
+            game.Teams.Add(team2);
+            Queue<int> diceRolls = new(new List<int> { 100, 100, 100, 100, 100 }); //Chance to hit roll, damage roll, critical chance roll
+
+
+            //Assert - Setup
+            Assert.AreEqual(1, game.TurnNumber);
+            Assert.AreEqual(2, game.Teams.Count);
+            Assert.AreEqual(50 * 50, game.Map.Length);
+            Assert.AreEqual("Good guys", game.Teams[0].Name);
+            Assert.AreEqual(1, game.Teams[0].Characters.Count);
+            Assert.AreEqual("Bad guys", game.Teams[1].Name);
+            Assert.AreEqual(1, game.Teams[1].Characters.Count);
+
+            //Act
+
+            //Turn 1 - Team 1 starts
+            //Fred cannot see Jeff, who is hiding behind cover
+            string mapString1 = fred.GetCharactersInViewMapString(game.Map, new List<Team> { team2 });
+            List<Character> characters = fred.GetCharactersInView(game.Map, new List<Team>() { team2 });
+            Assert.AreEqual(0, characters.Count);
+            string mapResult1 = @"
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o □ □ □ □ □ □ □ □ □ o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o □ □ □ □ □ □ □ o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o □ □ □ □ □ □ □ o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o □ □ □ □ □ o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o □ □ □ □ □ o o o o o o ■ P □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o □ □ □ o o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o □ □ □ o o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o □ o o o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o ■ o o o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o P o o o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+";
+            Assert.AreEqual(mapResult1, mapString1);
+
+            //Throw grenade in front of wall
+            Vector3 targetThrowingLocation = new(13, 0, 10);
+            EncounterResult encounter1 = Encounter.AttackCharacterWithAreaOfEffect(fred, fred.UtilityItemEquipped, team2.Characters, game.Map, diceRolls, targetThrowingLocation);
+            string log1 = @"
+Fred is attacking with area effect Grenade aimed at <13, 0, 10>
+Characters in affected area: Jeff
+Damage range: 3-4, (dice roll: 100)
+Critical chance: 0, (dice roll: 100)
+Critical damage range: 3-4, (dice roll: 100)
+4 damage dealt to character Jeff, HP is now 8
+10 XP added to character Fred, for a total of 10 XP
+Cover removed from <14, 0, 10>
+Cover removed from <14, 0, 8>
+Cover removed from <14, 0, 9>
+Cover removed from <14, 0, 11>
+Cover removed from <14, 0, 12>
+Cover removed from <14, 0, 7>
+Cover removed from <14, 0, 13>
+";
+            Assert.AreEqual(log1, encounter1.LogString);
+
+            string mapString2 = fred.GetCharactersInViewMapString(game.Map, new List<Team> { team2 });
+            List<Character> characters2 = fred.GetCharactersInView(game.Map, new List<Team>() { team2 });
+            Assert.AreEqual(1, characters2.Count);
+            string mapResult2 = @"
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o o □ o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o □ o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o o □ o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o □ o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o o □ o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ o o o o o o o □ o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ □ o o o o o o □ o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ o o o o o o □ o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ □ o o o o o □ o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+□ □ □ □ □ □ □ □ □ □ o o o o o □ o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o □ □ □ □ □ □ □ □ □ o o o o ■ o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o □ □ □ □ □ □ □ o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o □ □ □ □ □ □ □ o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o □ □ □ □ □ o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o □ □ □ □ □ o o o o o o o P o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o □ □ □ o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o □ □ □ o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o □ o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o ■ o o o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o P o o o o o o o o ■ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o o □ □ □ □ □ □ □ □ □ □ □ □ □ □ 
+";
+            Assert.AreEqual(mapResult2, mapString2);
+
+            //End of of battle
+
+            //Assert
         }
 
     }
