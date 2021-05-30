5b2676252ba71d2e5963af310fab05d5d085c437
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-05-30 13:09:41 -0400

    Added more environmental properties

--- src/Battle.Logic/Map/MapTileItem.cs
+++ src/Battle.Logic/Map/MapTileItem.cs
@@ -4,7 +4,16 @@
 //    {
 //        public string Name { get; set; }
 //        public int HitPoints { get; set; }
-//        public bool IsBurnable { get; set; }
+//        //public bool IsBurnable { get; set; }
+//        //public int BurnDamage { get; set; }
 //        public bool IsExplosive { get; set; }
+//        public int ExplosiveRange { get; set; }
+//        public int ExplosiveDamage { get; set; }
+//        //public bool IsTargetable { get; set; }
+
+//        //public void ProcessEnvironmentEffects()
+//        //{ 
+        
+//        //}
 //    }
 //}
