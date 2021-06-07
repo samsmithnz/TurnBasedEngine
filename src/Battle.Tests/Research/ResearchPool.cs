d11b38804cb3a88250949fcb574b1f210db8c713
Sam Smith (Microsoft) <samsmithnz@gmail.com>
2021-06-07 18:25:45 -0400

    created basic research

---
+++ src/Battle.Tests/Research/ResearchPool.cs
@@ -0,0 +1,47 @@
+﻿using Battle.Logic.Research;
+
+namespace Battle.Tests.Research
+{
+    public static class ResearchPool
+    {
+        public static ResearchItem CreateAdvancedWeapons()
+        {
+            return new ResearchItem()
+            {
+                Name = "Advanced weapons",
+                ResearchPrereq = null,
+                ItemPrereq = null,
+                DaysToComplete = 5,
+                DaysCompleted = 5,
+                IsComplete = true
+            };
+        }
+
+        public static ResearchItem CreateLasers()
+        {
+            return new ResearchItem()
+            {
+                Name = "Laser weapons",
+                ResearchPrereq = CreateAdvancedWeapons(),
+                ItemPrereq = null,
+                DaysToComplete = 5,
+                DaysCompleted = 3,
+                IsComplete = false
+            };
+        }
+
+        public static ResearchItem CreatePlasma()
+        {
+            return new ResearchItem()
+            {
+                Name = "Plasma weapons",
+                ResearchPrereq = CreateLasers(),
+                ItemPrereq = null,
+                DaysToComplete = 5,
+                DaysCompleted = 0,
+                IsComplete = false
+            };
+        }
+
+    }
+}
