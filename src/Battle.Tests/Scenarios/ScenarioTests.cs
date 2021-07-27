a85225fa64337d73f746c28bf5ccfeedfd2f0497
Sam Smith <samsmithnz@gmail.com>
2021-07-26 20:24:18 -0400

    IMproved code coverage

--- src/Battle.Tests/Scenarios/ScenarioTests.cs
+++ src/Battle.Tests/Scenarios/ScenarioTests.cs
@@ -4,13 +4,13 @@ using Battle.Logic.GameController;
 using Battle.Logic.Map;
 using Battle.Logic.Utility;
 using Battle.Tests.Characters;
-using Battle.Tests.Map;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 using System.Numerics;
 
 namespace Battle.Tests.Scenarios
 {
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
     [TestClass]
     [TestCategory("L0")]
     public class ScenarioTests
