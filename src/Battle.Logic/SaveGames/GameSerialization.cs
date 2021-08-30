6619071496acd9a8266d6b882d25c548d1cf2264
Sam Smith <samsmithnz@gmail.com>
2021-08-30 15:51:59 -0400

    fix to save

--- src/Battle.Logic/SaveGames/GameSerialization.cs
+++ src/Battle.Logic/SaveGames/GameSerialization.cs
@@ -5,7 +5,7 @@ using System.IO;
 
 namespace Battle.Logic.SaveGames
 {
-    public class GameSerialization
+    public static class GameSerialization
     {
         public static Mission LoadGame(string json)
         {
@@ -13,29 +13,30 @@ namespace Battle.Logic.SaveGames
             return mission;
         }
 
-        public static bool SaveGame(Mission mission)
+        public static string SaveGame(Mission mission)
         {
             string json = JsonConvert.SerializeObject(mission);
-            string systemPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            string path = systemPath + @"\my games\TBS\Saves\";
-            if (Directory.Exists(path) == false)
-            {
-                Directory.CreateDirectory(path);
-            }
-            int number = new DirectoryInfo(path).GetFiles().Length;
-            string fileName = $"Save{number:000}.json";
-            while (File.Exists(path + fileName) == true)
-            {
-                number++;
-                fileName = $"Save{number:000}.json";
-                if (number > 1000)
-                {
-                    //we don't want an infinite loop
-                    break;
-                }
-            }
-            File.WriteAllText(path + fileName, json);
-            return true;
+            return json;
+            //string systemPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            //string path = systemPath + @"\my games\TBS\Saves\";
+            //if (Directory.Exists(path) == false)
+            //{
+            //    Directory.CreateDirectory(path);
+            //}
+            //int number = new DirectoryInfo(path).GetFiles().Length;
+            //string fileName = $"Save{number:000}.json";
+            //while (File.Exists(path + fileName) == true)
+            //{
+            //    number++;
+            //    fileName = $"Save{number:000}.json";
+            //    if (number > 1000)
+            //    {
+            //        //we don't want an infinite loop
+            //        break;
+            //    }
+            //}
+            //File.WriteAllText(path + fileName, json);
+            //return true;
         }
     }
 }
