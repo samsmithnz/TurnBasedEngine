b9c12b6898d710a0371296de55b26ad10c17b000
Sam Smith <samsmithnz@gmail.com>
2021-09-09 09:43:11 -0400

    Added new test and test save files

---
+++ src/Battle.Tests/Map/TeamFieldOfViewTests.cs
@@ -0,0 +1,23 @@
+﻿using Battle.Logic.Map;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace Battle.Tests.Map
+{
+    [TestClass]
+    [TestCategory("L1")]
+    public class TeamFieldOfViewTests
+    {
+        [TestMethod]
+        public void TeamTestMultipleCharactersFOVTest()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+
+    }
+}
