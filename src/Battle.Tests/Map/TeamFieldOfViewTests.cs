2752fd482958e7bff810af32dcfa2a52696239d3
Sam Smith <samsmithnz@gmail.com>
2021-09-09 10:27:46 -0400

    Added link to saved file

--- src/Battle.Tests/Map/TeamFieldOfViewTests.cs
+++ src/Battle.Tests/Map/TeamFieldOfViewTests.cs
@@ -1,7 +1,11 @@
-﻿using Battle.Logic.Map;
+﻿using Battle.Logic.GameController;
+using Battle.Logic.Map;
+using Battle.Logic.SaveGames;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
+using System.IO;
 using System.Numerics;
+using System.Reflection;
 
 namespace Battle.Tests.Map
 {
@@ -9,14 +13,30 @@ namespace Battle.Tests.Map
     [TestCategory("L1")]
     public class TeamFieldOfViewTests
     {
+        private string _rootPath;
+
+        [TestInitialize]
+        public void GameSerializationStartUp()
+        {
+            _rootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        }
+
         [TestMethod]
         public void TeamTestMultipleCharactersFOVTest()
         {
             //Arrange
+            string path = _rootPath + @"\SaveGames\Saves\SaveFOV002.json";
 
             //Act
+            string fileContents;
+            using (var streamReader = new StreamReader(path))
+            {
+                fileContents = streamReader.ReadToEnd();
+            }
+            Mission mission = GameSerialization.LoadGame(fileContents);
 
             //Assert
+            Assert.IsNotNull(mission);
         }
 
     }
