2c236b2af18dc88429de290038d0e8adae6166eb
Sam Smith <samsmithnz@gmail.com>
2021-10-15 12:27:14 -0400

    Added new wrapping list functionality

---
+++ src/Battle.Logic/Utility/WrappingList.cs
@@ -0,0 +1,94 @@
+﻿using Battle.Logic.Characters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Battle.Logic.Utility
+{
+    public static class WrappingList
+    {
+        //Move to the next item in the character list with action points
+        public static int FindNextIndex(int index, List<Character> characters)
+        {
+            bool foundFreeCharacter = false;
+
+            index++;
+            if (index > characters.Count - 1)
+            {
+                index = 0;
+            }
+
+            for (int i = index; i <= characters.Count - 1; i++)
+            {
+                if (characters[i].ActionPointsCurrent > 0)
+                {
+                    foundFreeCharacter = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (foundFreeCharacter == false)
+            {
+                //look in the first half of the list
+                for (int i = 0; i <= index; i++)
+                {
+                    if (characters[i].ActionPointsCurrent > 0)
+                    {
+                        foundFreeCharacter = true;
+                        index = i;
+                        break;
+                    }
+                }
+            }
+
+            if (foundFreeCharacter == false)
+            {
+                index = -1;
+            }
+
+            return index;
+        }
+
+        //Move to the previous item in the character list with action points
+        public static int FindPreviousIndex(int index, List<Character> characters)
+        {
+            bool foundFreeCharacter = false;
+
+            index--;
+            if (index < 0)
+            {
+                index = characters.Count - 1;
+            }
+
+            for (int i = index; i >= 0; i--)
+            {
+                if (characters[i].ActionPointsCurrent > 0)
+                {
+                    foundFreeCharacter = true;
+                    index = i;
+                    break;
+                }
+            }
+            if (foundFreeCharacter == false)
+            {
+                //look in the second half of the list
+                for (int i = characters.Count - 1; i >= index; i--)
+                {
+                    if (characters[i].ActionPointsCurrent > 0)
+                    {
+                        foundFreeCharacter = true;
+                        index = i;
+                        break;
+                    }
+                }
+            }
+
+            if (foundFreeCharacter == false)
+            {
+                index = -1;
+            }
+
+            return index;
+        }
+    }
+}
