ac50179035a32b803d08fd1e5c77ad2f319f8557
Sam Smith <samsmithnz@gmail.com>
2021-10-31 09:04:16 -0400

    Added performance profiling console

--- src/Battle.Tests/Scenarios/AICrashTest.cs
+++ src/Battle.Tests/Scenarios/AICrashTest.cs
@@ -1,105 +1,113 @@
-﻿//using Battle.Logic.Characters;
-//using Battle.Logic.Encounters;
-//using Battle.Logic.Game;
-//using Battle.Logic.Map;
-//using Battle.Logic.SaveGames;
-//using Microsoft.VisualStudio.TestTools.UnitTesting;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Numerics;
-//using System.Reflection;
+﻿using Battle.Logic.Characters;
+using Battle.Logic.Encounters;
+using Battle.Logic.Game;
+using Battle.Logic.Map;
+using Battle.Logic.SaveGames;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Numerics;
+using System.Reflection;
 
-//namespace Battle.Tests.Scenarios
-//{
-//    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
-//    [TestClass]
-//    [TestCategory("L2")]
-//    public class AICrashTest
-//    {
-//        private string _rootPath;
+namespace Battle.Tests.Scenarios
+{
+    [System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+    [TestClass]
+    [TestCategory("L2")]
+    public class AICrashTest
+    {
+        private string _rootPath;
 
-//        [TestInitialize]
-//        public void GameSerializationStartUp()
-//        {
-//            _rootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-//        }
+        [TestInitialize]
+        public void GameSerializationStartUp()
+        {
+            _rootPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"/SaveGames/Saves/";
+        }
 
-//        [TestMethod]
-//        public void AICrashesOnFirstTurnTest()
-//        {
-//            //Arrange
-//            string path = _rootPath + @"\SaveGames\Saves\Save013.json";
+        [TestMethod]
+        public void AICrashesOnFirstTurnTest()
+        {
+            //Arrange
+            string path = _rootPath + "Save019.json";
 
-//            //Act
-//            string fileContents;
-//            using (var streamReader = new StreamReader(path))
-//            {
-//                fileContents = streamReader.ReadToEnd();
-//            }
-//            Mission mission = GameSerialization.LoadGame(fileContents);
-//            mission.StartMission();
-//            mission.MoveToNextTurn();
+            //Act
+            string fileContents;
+            using (var streamReader = new StreamReader(path))
+            {
+                fileContents = streamReader.ReadToEnd();
+            }
+            Mission mission = GameSerialization.LoadGame(fileContents);
+            mission.StartMission(); 
+            Character fred = mission.Teams[0].GetCharacter("Fred");
+            Character enemy1 = mission.Teams[1].Characters[0];
+            Character enemy2 = mission.Teams[1].Characters[1];
+            Team team1 = mission.Teams[0];
+            Team team2 = mission.Teams[1];
+            Assert.IsTrue(enemy1.HitpointsCurrent <= 0);
 
-//            AIAction aIAction = mission.CalculateAIAction(mission.Teams[1].Characters[0], mission.Teams);
-//            string mapString = aIAction.MapString;
-//            string mapStringExpected = @"
-//. . . . . . . . . . . □ . . ■ . . □ . . . . . . . . □ . . . . . ■ . . . . . . . . . ■ . . . ■ . . . 
-//. . . . . . . . . . . . . . . . . . . . ■ . . . ■ . . . . . . . . . . . . . . . . . . ■ . . . . □ . 
-//. . . . . . . . . . . . . . . . ■ . . . . . . . . . . . . ■ . . . . . . . . . . . . . . . . . . . . 
-//. . . . . . . . . . ■ . . . . . . . . ■ . . . . . . . . . □ ■ . . . . . . . . . . . . . . . . . . . 
-//. . . ■ . . ■ . . . □ . . . . . . . . . . □ . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
-//. . . . □ . . . . □ . . . . . . . . . . . . . . . . . . . . . . . . □ . . . . . . . . ■ . . . . . . 
-//. . . . . □ . . . . . . . . . . . . . . . . . . . □ . . . . . . . . . . . . . . . □ . . . . . . □ . 
-//. . . . ■ . . . . . . . . . □ . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . . . □ . . . 
-//. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . . ■ . . □ . . . . . . . . . . . 
-//. . . . . . . . . . . . . . . . . . . . . . □ . . . . □ . . □ . . . . . . . . . ■ . . . . . . . . . 
-//. . . . . . . . . . . □ . . . . □ . . . . . . . . . . . . . . . . . . . . □ . . . . . . ■ . . . . . 
-//. . . . . . . □ . . . . . . . . . . . . . . . . . . . . . . . ■ . . . . . . ■ . . . . . . . . . . . 
-//. . . . . ■ . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . . . . . . . 
-//. . . . . . . . . . . . . . . . . . . 1 ■ . . . ■ . . . . . . ■ . . . ■ . . . . . . . . . . . . . . 
-//. . . . . . ■ . . . . . . ■ . . . 1 1 0 0 0 . . . . . □ . . . . . . . . . . . . . . . . . . . . . . 
-//. □ . . . . . . . . . . . . . 1 1 1 0 0 1 1 0 0 . . . . . . ■ . . . . . . . . . . . ■ . . ■ . . . . 
-//. . . . . . . . . . . . 1 1 1 1 1 0 0 0 □ 1 0 0 2 ■ P □ . . . . . . . . . . . . . . . . . . . . . . 
-//. . . . . . . . ■ . 1 4 3 3 3 3 3 0 0 1 □ 1 0 0 □ 4 1 1 1 . □ . . . . . . . . ■ . □ . . . . . . . . 
-//. . . . □ . □ . . 1 1 ■ 3 3 3 3 1 1 3 1 1 0 0 1 1 0 0 1 1 1 1 . . . . . . . . . . ■ . . . . . . . . 
-//. . . . . . . . 1 1 1 3 3 3 3 1 1 1 □ 3 1 1 3 1 1 3 1 0 0 1 3 . . . . . . . . . . . . . . . . . □ . 
-//. . . . . . . 1 1 1 4 3 3 3 1 1 1 1 1 1 1 3 3 1 □ □ 1 0 0 1 □ 3 . . . . . . . . . . . . . . . ■ . . 
-//□ . . . . . . 1 1 1 ■ 3 3 1 1 1 1 1 1 1 3 1 1 3 1 1 0 0 ■ 2 0 □ . ■ . . . ■ . . . ■ . . . . . . ■ . 
-//. . . . . . 1 1 1 1 1 3 3 1 1 3 1 1 4 3 3 3 3 1 1 1 1 1 0 0 0 2 1 . . . . . . . □ . . □ . . . . . . 
-//. . . . . . 1 1 1 1 3 3 1 4 3 1 3 1 ■ 6 1 1 1 1 1 3 3 0 0 0 0 ■ 4 . ■ . . . □ . □ . . . . . . . . . 
-//. . . . . . . 3 1 4 3 1 4 ■ 1 1 3 1 3 3 1 1 4 1 3 3 □ 1 0 0 2 4 1 . . . . . . . . . . . . . . . . . 
-//. . . . . 1 1 □ 1 ■ 1 1 □ 1 4 3 4 1 3 3 1 4 ■ 6 3 3 0 1 0 0 ■ ■ 2 1 . . . □ . . . . . . . . . . ■ . 
-//. . . . . 1 1 1 1 ■ 1 1 1 1 ■ 3 ■ 6 3 1 1 ■ 6 5 5 2 0 □ 1 0 0 0 ■ 2 . . . . . □ . . . □ . . . . . . 
-//□ . . . □ 1 1 1 1 1 1 1 1 1 1 3 3 3 3 1 5 5 5 5 2 2 2 0 0 0 0 1 0 0 0 . . . . . . . . . ■ . . . . . 
-//. . . . 1 1 1 1 1 1 1 1 1 1 1 3 3 3 3 3 7 5 5 5 2 2 2 0 0 0 0 0 1 1 1 . . . . . . . . . . . . . . . 
-//■ . . 1 0 1 1 1 1 1 1 3 1 1 1 5 3 5 3 P □ 7 5 2 2 2 2 0 0 0 0 1 1 0 0 . . . . . . . . . . . . . . . 
-//. ■ . . 0 1 1 1 1 1 1 1 1 1 3 □ 7 5 3 5 5 5 3 2 2 2 2 0 0 0 1 0 □ 1 . . . . . . . . . . . . . . . . 
-//■ . . . 0 1 1 1 1 1 1 1 1 3 5 5 5 3 5 5 5 3 2 2 3 2 2 0 0 1 0 0 0 1 0 . . . . . . □ . . □ . . . . . 
-//. . . . . 0 1 1 1 1 1 3 5 3 5 5 5 3 5 5 3 2 2 2 □ 3 2 3 0 0 0 0 0 □ . . . . . . . . . . . □ . . . . 
-//. . . . . 0 1 1 1 3 3 3 □ 3 5 5 6 5 5 5 3 2 0 0 0 0 ■ 2 0 0 0 0 0 ■ . . . ■ . . . . □ . . . . . . . 
-//. . . . . . 1 3 3 3 3 3 3 3 5 5 ■ 6 3 1 2 2 2 2 2 0 0 □ 1 0 0 0 6 . . . . . . . . . . . □ ■ . . . . 
-//. . . . . □ 1 3 3 3 3 3 3 3 3 5 5 5 3 3 2 2 2 2 0 0 0 0 0 0 3 1 ■ . . . . . . . . . . . . . □ ■ . . 
-//. . P ■ . . 0 1 3 □ 3 3 □ 3 3 □ 5 3 3 2 2 2 0 0 0 0 0 0 0 ■ 4 □ 1 . . . . . . . . . . . . . . . . . 
-//. . . . . . . 1 3 3 3 3 3 3 3 3 3 1 1 2 0 0 0 0 0 0 0 0 □ 1 1 3 . . . . □ . . . . . □ . . . . . . . 
-//. . . . . . . ■ 4 3 3 3 3 3 3 3 1 1 0 0 0 0 0 0 0 0 0 1 1 1 ■ 6 □ . . . . . . . ■ . □ . . . . . . . 
-//. . . . □ □ . . ■ 1 3 3 3 3 3 1 1 1 0 0 0 0 0 0 0 1 1 1 3 3 1 . . . . . . . . ■ . . . . . . □ . . . 
-//. . . . . . . . . 0 1 3 3 3 1 1 1 0 0 0 0 0 0 0 1 1 3 1 1 1 1 . . . . . . . . . . . . □ . . . . . . 
-//. . . □ . □ . . . □ 0 ■ 6 3 1 1 1 0 0 0 0 0 1 1 ■ □ 3 1 0 . . . . . . . . . . . . . . . . . . . . . 
-//. . . . . . . . □ . . . □ 1 1 1 0 0 0 0 1 1 1 1 1 . □ . . . . . . . . . . . . . . . . . . . . . . . 
-//. . . □ . □ . ■ . . . . . . . 1 0 0 1 1 1 0 0 0 . . . . . . . . . . . . . . . . . . . . . . . . □ . 
-//. . . . . . . . . . . . . . . . . 1 0 0 0 0 . . . . . . . . . . . ■ . □ . . . ■ . . . . . . . . . . 
-//. . . . . . . . . ■ ■ . . . ■ . . . . 1 . . . . . . ■ . . . . . □ . . . . . . . . ■ . . . . . ■ . . 
-//. . . . . . . . . . . . . . P . . . . . . . . . . . ■ . . . . . . . . . . . . . . . . . . . . . . . 
-//. . . . . . . . . P . . . . . . □ . . . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . □ . 
-//. . . . . . . . . . . . . . . . . . . . . . . . . . . . . □ . □ . . . . . . . . . . . . . . . . ■ . 
-//";
+            //Move to enemy turn
+            mission.MoveToNextTurn();
 
-//            //Assert
-//            Assert.AreEqual(ActionTypeEnum.MoveThenAttack, aIAction.ActionType);
-//            //Assert.AreEqual(6, aIAction.Score);
-//            Assert.AreEqual(new Vector3(19, 0, 19), aIAction.StartLocation);
-//            Assert.AreEqual(new Vector3(16, 0, 23), aIAction.EndLocation);
-//            //Assert.AreEqual(mapStringExpected, mapString);
+            AIAction aIAction = mission.CalculateAIAction(enemy2, team2, team1);
+            string mapString = aIAction.MapString;
+            string mapStringExpected = @"
+. . . . . . . . . . . □ . . ■ . . □ . . . . . . . . □ . . . . . ■ . . . . . . . . . ■ . . . ■ . . . 
+. . . . . . . . . . . . . . . . . . . . ■ . . . ■ 4 1 1 1 . . . . . . . . . . . . . . ■ . . . . □ . 
+. . . . . . . . . . . . . . . . ■ . . . . . 1 1 1 1 1 1 1 ■ . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . ■ . . . . . . . . ■ 4 3 1 1 1 1 1 1 1 □ ■ 4 1 1 . . . . . . . . . . . . . . . . 
+. . . ■ . . ■ . . . □ . . . . . . 1 1 1 1 □ 3 1 1 1 1 1 1 1 1 1 1 1 3 1 . . . . . . . . . . . . . . 
+. . . . □ . . . . □ . . . . . 1 1 1 1 1 1 1 1 1 1 3 1 1 1 1 1 1 1 1 □ 3 1 . . . . . . ■ . . . . . . 
+. . . . . □ . . . . . . . . . 1 1 1 1 1 1 1 1 1 1 □ 3 1 1 1 1 1 1 1 1 1 1 1 . . . □ . . . . . . □ . 
+. . . . ■ . . . . . . . . . □ 3 1 1 1 1 1 1 1 1 1 1 1 1 1 1 4 1 1 1 1 4 1 1 3 . ■ . . . . . □ . . . 
+. . . . . . . . . . . . . . 1 1 1 1 1 1 1 1 3 1 1 1 0 3 1 1 ■ 4 1 1 1 ■ 4 1 □ . . . . . . . . . . . 
+. . . . . . . . . . . . . 1 1 1 3 1 1 1 1 1 □ 3 0 0 0 □ 0 1 □ 3 1 1 1 1 1 3 1 1 ■ . . . . . . . . . 
+. . . . . . . . . . . □ . 0 1 1 □ 3 1 1 1 1 0 0 0 0 0 0 0 0 0 4 1 1 1 1 1 □ 4 1 . . . . ■ . . . . . 
+. . . . . . . □ . . . . . 0 1 1 1 1 1 1 1 0 0 0 0 0 0 0 0 0 0 ■ 4 1 1 1 1 1 ■ 4 . . . . . . . . . . 
+. . . . . ■ . . . . . . 0 1 1 1 1 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 4 1 1 1 1 ■ . . . . . . . . . 
+. . . . . . . . . . . . 0 4 1 1 1 0 0 0 ■ 0 0 0 ■ 0 0 0 0 0 0 ■ 0 1 1 ■ 4 1 1 1 1 . . . . . . . . . 
+. . . . . . ■ . . . . 0 0 ■ 4 1 0 0 0 0 0 0 2 2 0 0 0 □ 0 0 0 0 0 0 1 1 1 1 1 1 1 1 . . . . . . . . 
+. □ . . . . . . . . . 0 1 1 1 1 0 0 0 2 2 0 0 2 0 0 0 0 0 0 ■ 0 0 1 1 1 1 1 1 1 1 . ■ . . ■ . . . . 
+. . . . . . . . . . . . 1 1 1 0 0 0 1 1 □ 2 2 2 0 ■ P □ 0 0 0 0 0 0 1 1 1 1 1 4 1 3 . . . . . . . . 
+. . . . . . . . ■ . . . 1 1 1 0 0 0 1 0 □ 5 5 0 □ 0 0 0 0 0 □ 0 0 1 1 1 1 1 1 ■ 4 □ . . . . . . . . 
+. . . . □ . □ . . . . ■ 6 1 0 0 0 1 1 0 0 3 3 3 2 0 2 2 0 0 0 0 0 0 1 1 1 1 1 1 1 ■ . . . . . . . . 
+. . . . . . . . . . . . 3 0 0 0 1 0 □ 1 2 3 3 0 3 3 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 . . . . . . . □ . 
+. . . . . . . . . . . . 1 0 0 1 1 0 0 1 3 3 0 3 □ □ 2 3 5 0 □ 0 0 4 1 1 1 4 1 1 1 . . . . . . ■ . . 
+□ . . . . . . . . . ■ . . 0 0 1 0 1 1 0 0 0 3 3 3 0 3 0 ■ 0 0 □ 4 ■ 4 1 1 ■ 4 1 . ■ . . . . . . ■ . 
+. . . . . . . . . . . . . 0 1 0 1 1 1 0 0 0 3 3 0 3 0 0 3 3 0 4 1 1 4 1 1 1 3 1 □ . . □ . . . . . . 
+. . . . . . . . . . . . . 2 1 0 1 1 . 0 0 0 0 0 0 0 2 3 3 0 0 ■ 4 1 ■ 4 1 1 □ 3 □ . . . . . . . . . 
+. . . . . . . . . . . . . ■ 2 1 1 1 0 0 0 0 2 0 0 0 □ 0 0 0 4 4 1 1 1 1 1 3 1 . . . . . . . . . . . 
+. . . . . . . □ . ■ . . □ . 2 3 4 0 0 0 0 2 ■ 2 0 0 0 0 0 0 ■ ■ 4 1 1 1 1 □ 3 . . . . . . . . . ■ . 
+. . . . . . . . . ■ . . . . ■ 4 ■ 0 0 0 0 ■ 0 0 0 0 0 □ 1 0 0 0 ■ 4 1 1 1 1 . □ . . . □ . . . . . . 
+□ . . . □ . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1 1 1 . . . . . . . ■ . . . . . 
+. . . . . . . . . . . . . . . . . 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 1 1 . . . . . . . . . . . . . . 
+■ . . . . . . . . . . . . . . . . . . 0 □ 1 0 0 0 1 1 0 1 1 1 0 . 1 . . . . . . . . . . . . . . . . 
+. ■ . . . . . . . . . . . . . □ . . . . . . 0 0 1 1 3 1 1 0 0 . □ . . . . . . . . . . . . . . . . . 
+■ . . . . . . . . . . . . . . . . . . . . . . . 5 3 3 . 0 . . . . . . . . . . . . □ . . □ . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . □ . . . . . . . . □ . . . . . . . . . . . □ . . . . 
+. . . . . . . . . . . . □ . . . . . . . . . . . . . ■ . . . . . . ■ . . . ■ . . . . □ . . . . . . . 
+. . . . . . . . . . . . . . . . ■ . . . . . . . . . . □ . . . . . . . . . . . . . . . . □ ■ . . . . 
+. . . . . □ . . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . . . . . . . . . . . □ ■ . . 
+. . . ■ . . . . . □ . . □ . . □ . . . . . . . . . . . . . ■ . □ . . . . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . □ . . . . . . . □ . . . . . □ . . . . . . . 
+. . . . . . . ■ . . . . . . . . . . . . . . . . . . . . . . ■ . □ . . . . . . . ■ . □ . . . . . . . 
+. . . P □ □ . . ■ . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . . . . □ . . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . □ . . . . . . 
+. . . □ . □ . . . □ . ■ . . . . . . . . . . . . ■ □ . . . . . . . . . . . . . . . . . . . . . . . . 
+. . . . . . . . □ . . . □ . . . . . . . . . . . . . □ . . . . . . . . . . . . . . . . . . . . . . . 
+. . . □ . □ . ■ . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . □ . 
+. . . . . . . P . . . . . . . . . . . . . . . . . . . . . . . . . ■ . □ . . . ■ . . . . . . . . . . 
+. . . . . . . . . ■ ■ . . . ■ . . . . . . . . . . . ■ . . . . . □ . . . . . . . . ■ . . . . . ■ . . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . . . . . . . . . . . . . . . . . . . . . 
+. P . . . . . . . . . . . . . . □ . . . . . . . . . . . . . . . . . . . . . . . . . . . ■ . . . □ . 
+. . . . . . . . . . . . . . . . . . . . . . . . . . . . . □ . □ . . . . . . . . . . . . . . . . ■ . 
+";
+
+            //Assert
+            Assert.AreEqual(ActionTypeEnum.MoveThenAttack, aIAction.ActionType);
+            Assert.AreEqual(5, aIAction.Score);
+            Assert.AreEqual(new Vector3(26, 0, 32), aIAction.StartLocation);
+            Assert.AreEqual(new Vector3(28, 0, 28), aIAction.EndLocation);
+            Assert.AreEqual(mapStringExpected, mapString);
 
 //            //Act
 //            mission.MoveCharacter(mission.Teams[1].Characters[0],
@@ -206,6 +214,6 @@
 //                mission.Teams[1],
 //                mission.Teams[0],
 //                aIAction2.EndLocation);
-//        }
-//    }
-//}
+        }
+    }
+}
